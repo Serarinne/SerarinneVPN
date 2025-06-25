185ed9d390faaf2e358f0b15860a43f38e2eac18
Serarinne <160508714+Serarinne@users.noreply.github.com>
2025-06-25 21:57:22 +0700

    Update user-bandwidth.sh

--- bash/user-bandwidth.sh
+++ bash/user-bandwidth.sh
@@ -13,12 +13,20 @@ XRAY_DATA () {
     do
         USER_USAGE=$(grep -E "^${USER}#" "/root/serarinne/user-usage" | awk '{ gsub("#", " ") ; print $0 }' | cut -d ' ' -f 2)
 
-        echo $USER $USER_USAGE | numfmt --field=2 --format "%.2f" --suffix=B --to=iec | awk '{ gsub(" ", "#") ; print $0 }'
+        LIMITED_USER=$(grep -E ".*# ${USER} Backend # ${USER} Limit" "/etc/haproxy/haproxy.cfg" | wc -l)
+        if [[ $LIMITED_USER -gt 0 ]]; then
+            USER_STATUS="#DIBATASI"
+        else
+            USER_STATUS="#"
+        fi
+
+        echo $USER $USER_USAGE $USER_STATUS | numfmt --field=2 --format "%.2f" --suffix=B --to=iec | awk '{ gsub(" ", "#") ; print $0 }'
     done
 }
 
+user-usage > /dev/null 2>&1
 USERS_DATA=$(XRAY_DATA $1)
-echo "USER#USAGE ${USERS_DATA}" | tr ' ' '\n' | tr '#' ' ' | column -t
+echo "USER#USAGE#STATUS ${USERS_DATA}" | tr ' ' '\n' | tr '#' ' ' | column -t
 echo ""
 read -p "$(echo -e "Back")"
 menu
