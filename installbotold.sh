42f7d63376c481442b84072574a3931c7473bcc4
Serarinne <160508714+Serarinne@users.noreply.github.com>
2025-07-24 11:44:32 +0700

    Create installbotold.sh

---
+++ installbotold.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+export SERVER_SCRIPT="https://raw.githubusercontent.com/Serarinne/SerarinneVPN/main"
+export SERVER_IP=$(cat /root/serarinne/ip)
+clear
+echo -e "-----------------------------------------"
+echo -e "|              Setup Bot                |"
+echo -e "-----------------------------------------"
+echo -e ""
+read -rp "WHAPI TOKEN : " WHAPI_TOKEN
+
+rm -rf /root/serarinne/bot
+mkdir /root/serarinne/bot
+wget -O /root/serarinne/bot/index.py "${SERVER_SCRIPT}/bot/index.py" && chmod +x /root/serarinne/bot/index.py
+
+apt install python3 python3-pip python3-flask python3-full -y
+pip install --break-system-packages Flask
+pip install --break-system-packages requests
+pip install --break-system-packages jsonify
+pip install --break-system-packages requests-toolbelt
+pip install --break-system-packages python-dotenv
+pip install --break-system-packages spur
+
+cat > /root/serarinne/bot/.env <<-END
+TOKEN=${WHAPI_TOKEN}
+API_URL=https://gate.whapi.cloud
+BOT_URL=http://${SERVER_IP}:5000/hook
+IPSVR=${SERVER_IP}
+PORT=5000
+END
+
+cat > /etc/systemd/system/panelbot.service <<-END
+[Unit]
+Description=Bot WA
+
+[Service]
+ExecStart=nohup python3 /root/serarinne/bot/index.py &
+Type=oneshot
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+END
+
+wget -O /usr/bin/bot-user-list "${SERVER_SCRIPT}/bash/bot-user-list.sh" && chmod +x /usr/bin/bot-user-list
+wget -O /usr/bin/bot-add-user "${SERVER_SCRIPT}/bash/bot-add-user.sh" && chmod +x /usr/bin/bot-add-user
+wget -O /usr/bin/bot-user-status "${SERVER_SCRIPT}/bash/bot-user-status.sh" && chmod +x /usr/bin/bot-user-status
+
+chmod +x /etc/systemd/system/panelbot.service
+systemctl enable panelbot.service
+systemctl start panelbot.service
+systemctl restart panelbot.service
+rm -f /root/installbot.sh
+
+echo "0 1 * * * root systemctl restart panelbot.service" >> /etc/crontab
