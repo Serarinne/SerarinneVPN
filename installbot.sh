3307f8ed9269b94d9e04ead253393f3fac7eb01a
Serarinne <160508714+Serarinne@users.noreply.github.com>
2025-07-24 11:47:51 +0700

    Update installbot.sh

--- installbot.sh
+++ installbot.sh
@@ -6,7 +6,6 @@ echo -e "-----------------------------------------"
 echo -e "|              Setup Bot                |"
 echo -e "-----------------------------------------"
 echo -e ""
-read -rp "WHAPI TOKEN : " WHAPI_TOKEN
 
 wget -O /usr/bin/bot-add-user "${SERVER_SCRIPT}/bash/bot-add-user.sh" && chmod +x /usr/bin/bot-add-user
 wget -O /usr/bin/bot-delete-user "${SERVER_SCRIPT}/bash/bot-delete-user.sh" && chmod +x /usr/bin/bot-delete-user
